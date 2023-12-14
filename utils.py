368ba8a8ed2bb526c719b489acf61a669d9826d1
pradipganesh61 <113200973+pradipganesh61@users.noreply.github.com>
2023-12-14 14:38:39 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,8 +5,7 @@ import sys
 
 
 def import_train_configuration(config_file):
-    #configuration model
-    content = configparser.ConfigParser()
+    content = configparser.ConfigParser()                           #model configuration
     content.read(config_file)
     config = {}
     config['gui'] = content['simulation'].getboolean('gui')
@@ -29,9 +28,7 @@ def import_train_configuration(config_file):
     config['sumocfg_file_name'] = content['dir']['sumocfg_file_name']
     return config
 
-
 def import_test_configuration(config_file):
-    
     content = configparser.ConfigParser()
     content.read(config_file)
     config = {}
@@ -48,10 +45,7 @@ def import_test_configuration(config_file):
     config['model_to_test'] = content['dir'].getint('model_to_test') 
     return config
 
-
 def set_sumo(gui, sumocfg_file_name, max_steps):
-    
-
     # sumo things - we need to import python modules from the $SUMO_HOME/tools directory
     if 'SUMO_HOME' in os.environ:
         tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -67,32 +61,23 @@ def set_sumo(gui, sumocfg_file_name, max_steps):
  
     # setting the cmd command to run sumo at simulation time
     sumo_cmd = [sumoBinary, "-c", os.path.join('intersection', sumocfg_file_name), "--no-step-log", "true", "--waiting-time-memory", str(max_steps)]
-
     return sumo_cmd
 
-
 def set_train_path(models_path_name):
-    
-    
     models_path = os.path.join(os.getcwd(), models_path_name, '')
     os.makedirs(os.path.dirname(models_path), exist_ok=True)
-
     dir_content = os.listdir(models_path)
     if dir_content:
         previous_versions = [int(name.split("_")[1]) for name in dir_content]
         new_version = str(max(previous_versions) + 1)
     else:
         new_version = '1'
-
     data_path = os.path.join(models_path, 'model_'+new_version, '')
     os.makedirs(os.path.dirname(data_path), exist_ok=True)
     return data_path 
 
-
 def set_test_path(models_path_name, model_n):
-
     model_folder_path = os.path.join(os.getcwd(), models_path_name, 'model_'+str(model_n), '')
-
     if os.path.isdir(model_folder_path):    
         plot_path = os.path.join(model_folder_path, 'test', '')
         os.makedirs(os.path.dirname(plot_path), exist_ok=True)
