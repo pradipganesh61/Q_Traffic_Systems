b952dabd96b771eb8e828c4f7455eb297b27b961
pradipganesh61 <113200973+pradipganesh61@users.noreply.github.com>
2023-12-14 14:33:38 -0600

    Update testing_simulation.py

--- testing_simulation.py
+++ testing_simulation.py
@@ -5,13 +5,13 @@ import timeit
 import os
 
 # phase codes based on environment.net.xml
-PHASE_NS_GREEN = 0  # action 0 code 00
+PHASE_NS_GREEN = 0                         # action 0 code 00
 PHASE_NS_YELLOW = 1
-PHASE_NSL_GREEN = 2  # action 1 code 01
+PHASE_NSL_GREEN = 2                        # action 1 code 01
 PHASE_NSL_YELLOW = 3
-PHASE_EW_GREEN = 4  # action 2 code 10
+PHASE_EW_GREEN = 4                         # action 2 code 10
 PHASE_EW_YELLOW = 5
-PHASE_EWL_GREEN = 6  # action 3 code 11
+PHASE_EWL_GREEN = 6                        # action 3 code 11
 PHASE_EWL_YELLOW = 7
 
 
@@ -29,10 +29,8 @@ class Simulation:
         self._reward_episode = []
         self._queue_length_episode = []
 
-
     def run(self, episode):
         start_time = timeit.default_timer()
-
         # first, generate the route file for this simulation and set up sumo
         self._TrafficGen.generate_routefile(seed=episode)
         traci.start(self._sumo_cmd)
@@ -43,13 +41,9 @@ class Simulation:
         self._waiting_times = {}
         old_total_wait = 0
         old_action = -1 # dummy init
-
         while self._step < self._max_steps:
-
-            # get current state of the intersection
-            current_state = self._get_state()
-
-            # calculate reward of previous action: (change in cumulative waiting time between actions)
+            current_state = self._get_state()                                     # get current state of the intersection
+            # calculate reward of previous action => change in cumulative waiting time between actions
             # waiting time = seconds waited by a car since the spawn in the environment, cumulated for every car in incoming lanes
             current_total_wait = self._collect_waiting_times()
             reward = old_total_wait - current_total_wait
@@ -69,15 +63,12 @@ class Simulation:
             # saving variables for later & accumulate reward
             old_action = action
             old_total_wait = current_total_wait
-
             self._reward_episode.append(reward)
-
+            
         #print("Total reward:", np.sum(self._reward_episode))
         traci.close()
         simulation_time = round(timeit.default_timer() - start_time, 1)
-
         return simulation_time
-
 
     def _simulate(self, steps_todo):
         #Proceed with the simulation in sumo        
@@ -90,7 +81,6 @@ class Simulation:
             steps_todo -= 1
             queue_length = self._get_queue_length() 
             self._queue_length_episode.append(queue_length)
-
 
     def _collect_waiting_times(self):
         #Retrieve the waiting time of every car in the incoming roads
@@ -112,12 +102,10 @@ class Simulation:
         #Pick the best action known based on the current state of the env
         return np.argmax(self._Model.predict_one(state))
 
-
     def _set_yellow_phase(self, old_action):
         #Activate the correct yellow light combination in sumo
         yellow_phase_code = old_action * 2 + 1 # obtain the yellow phase code, based on the old action (ref on environment.net.xml)
         traci.trafficlight.setPhase("TL", yellow_phase_code)
-
 
     def _set_green_phase(self, action_number):
         #Activate the correct green light combination in sumo
@@ -130,7 +118,6 @@ class Simulation:
         elif action_number == 3:
             traci.trafficlight.setPhase("TL", PHASE_EWL_GREEN)
 
-
     def _get_queue_length(self):
         #Retrieve the number of cars with speed = 0 in every incoming lane
         halt_N = traci.edge.getLastStepHaltingNumber("N2TL")
@@ -139,7 +126,6 @@ class Simulation:
         halt_W = traci.edge.getLastStepHaltingNumber("W2TL")
         queue_length = halt_N + halt_S + halt_E + halt_W
         return queue_length
-
 
     def _get_state(self):
         #Retrieve the state of the intersection from sumo, in the form of cell occupancy
@@ -207,7 +193,6 @@ class Simulation:
                 state[car_position] = 1  # write the position of the car car_id in the state array in the form of "cell occupied"
 
         return state
-
 
     @property
     def queue_length_episode(self):
